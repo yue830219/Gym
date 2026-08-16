bea265af571fdc4d29314367f7274186277003f2
Local Version Snapshot <local-version@localhost>
2026-08-16 23:33:34 +0800

    鎖定訓練日曆頁面捲動

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gym-assistant-v12';
+const CACHE_NAME = 'gym-assistant-v13';
 const CORE_ASSETS = [
   './', './index.html', './manifest.json', './service-worker.js', './music.mp3',
   './favicon.png', './apple-touch-icon.png',
