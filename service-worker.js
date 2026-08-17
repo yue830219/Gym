6dc03eb7e64373e61b8ee3ce143a84a710e89f67
Local Version Snapshot <local-version@localhost>
2026-08-17 18:20:32 +0800

    將訓練日期與編輯靠左排列

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gym-assistant-v27';
+const CACHE_NAME = 'gym-assistant-v28';
 const CORE_ASSETS = [
   './', './index.html', './manifest.json', './service-worker.js', './music.mp3',
   './favicon.png', './apple-touch-icon.png',
